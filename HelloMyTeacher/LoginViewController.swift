c01dc04e99883f259ad2b30f05cc8b8c22a1c7a6
Dmitry <divinedavidbrown@yandex.ru>
2023-04-02 20:38:15 +0300

    add alert with text

--- HelloMyTeacher/LoginViewController.swift
+++ HelloMyTeacher/LoginViewController.swift
@@ -12,32 +12,49 @@ final class LoginViewController: UIViewController {
     @IBOutlet weak var userNameTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let greetingVC = segue.destination as? GreetingViewController else { return }
         greetingVC.username = userNameTF.text
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        userNameTF.text = ""
+        passwordTF.text = ""
     }
 
     @IBAction func loginButtonTapped() {
         if userNameTF.text == "Alexey" && passwordTF.text == "Efimov" {
             return
         } else {
-            showAlert(withTitle: "Неправильно", andMessage: "Неправильно")
+            showAlert(
+                withTitle: "Invalid login or password",
+                andMessage: "Please, enter correct login and password"
+            )
         }
     }
     
-    final private func showAlert(withTitle title: String, andMessage message: String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    @IBAction func forgotUserNameButtonPressed() {
+        showAlert(withTitle: "Oops!", andMessage: "Your name is Alexey 👨‍💻")
+    }
+    
+    @IBAction func forgotPasswordButtonPressed() {
+        showAlert(withTitle: "Oops!", andMessage: "Your password is Efimov 👨‍💻")
+    }
+    
+    private func showAlert(
+        withTitle title: String,
+        andMessage message: String
+    ) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
         let okAction = UIAlertAction(title: "OK", style: .default) { _ in
             self.passwordTF.text = ""
         }
         alert.addAction(okAction)
         present(alert, animated: true)
     }
-    
 }
 
