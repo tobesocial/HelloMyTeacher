fb216f15dbce67b05b36ec6bd31b42a3a0020235
Dmitry <divinedavidbrown@yandex.ru>
2023-04-02 17:56:35 +0300

    add new VC for view with greeting

---
+++ HelloMyTeacher/GreetingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingViewController.swift
+//  HelloMyTeacher
+//
+//  Created by Дмитрий Федоров on 02.04.2023.
+//
+
+import UIKit
+
+class GreetingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
